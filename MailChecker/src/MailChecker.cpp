2e564cad4ab1fe6540cab96177001300fee6014f
Flavien Moullec <flavien.moullec@laposte.net>
2014-08-11 22:45:02 +0200

    CMakeLists.txt file added

---
+++ MailChecker/src/MailChecker.cpp
@@ -0,0 +1,21 @@
+/*
+ * MailChecker.cpp
+ *
+ *  Created on: Aug 11, 2014
+ *      Author: flavien
+ */
+
+#include <iostream>
+using namespace std;
+
+/**
+ *
+ * \brief : Main function
+ *
+ */
+int main()
+{
+	//TODO
+	std::cout << "To begin :)" << std::endl;
+	return 0;
+}
