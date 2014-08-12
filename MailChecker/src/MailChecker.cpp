d6fbdb753fa622159becaed6d165747482a01ebc
Flavien Moullec <flavien.moullec@laposte.net>
2014-08-12 22:12:01 +0200

    Structure of the application updated

--- MailChecker/src/MailChecker.cpp
+++ MailChecker/src/MailChecker.cpp
@@ -2,20 +2,58 @@
  * MailChecker.cpp
  *
  *  Created on: Aug 11, 2014
- *      Author: flavien
+ *  Author: Flavien Moullec
  */
 
 #include <iostream>
-using namespace std;
+#include <vector>
 
 /**
  *
- * \brief : Main function
- *
  */
-int main()
+class MailChecker
 {
+public:
+	MailChecker(std::string p_mail, std::string p_password,
+			std::string p_protocol)
+	{
+
+	}
+	virtual ~MailChecker()
+	{
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	std::vector<std::string> * checkMail()
+	{
+		//TODO
+		return NULL;
+	}
+
+private:
+	std::string m_mail;
+	std::string m_password;
+	std::string m_protocol;
+};
+
+int main(int argc, char *argv[])
+{
+	bool exit = false;
+
+	// Retreives parameters from the command line
 	//TODO
-	std::cout << "To begin :)" << std::endl;
+
+	// Create the MailChecker object
+	//TODO
+
+	// Check the mail periodically
+	while (!exit)
+	{
+		//TODO
+	}
+
 	return 0;
 }
